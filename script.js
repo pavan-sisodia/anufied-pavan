39ddf156075ce80df3d01ccc260838681b8850ea
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2023-10-26 17:17:57 +0530

    animation update

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// const tl = gsap.timeline({paused: true});
-// tl.from("#profile", {scale: 0.7, autoAlpha: 0});
 
-
-// const startY = innerHeight / 10;
-// const finishDistance = innerHeight / 5;
-// function animateAtScrollPos() {
-//   tl.progress((scrollY - startY) / finishDistance);
-// }
-// document.addEventListener("scroll", animateAtScrollPos);
 gsap.from("#profile",{
     delay:0.4,
     opacity:0,
